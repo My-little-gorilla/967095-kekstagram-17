6e5151bd9109fe1ecc01c204a135c0eac0760941
name <Brunogoat666@gmail.com>
2019-06-16 23:14:46 +0300

    kekstgrm начало

---
+++ js/main.js
@@ -0,0 +1,45 @@
+var randomNumber = function (min, max) {
+      randomNumber = min + Math.random() * (max + 1 - min);
+      raundedRandomNumber = Math.floor(randomNumber);
+      return Math.floor;
+  }
+
+  // var quantityDescriptionPhoto = randomNumber(15, 200);
+  // console.log(quantityDescriptionPhoto);
+
+var takePicture = function(){
+  for (var j = 0; j < 25; j++){
+  var url = ['photos/' + [j] + '.jpg'];
+  return url;
+  }
+};
+
+var likes = function(){
+  for (var i = 0; i < 25; i++) {
+    var likesArray = [i];
+
+  }
+};
+likes();
+
+var names = [
+'Иван',  'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита','Вашингтон'
+];
+
+
+  var comments = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+ ];
+var avatars = [
+  'img/avatar-1.svg',
+  'img/avatar-2.svg',
+  'img/avatar-3.svg',
+  'img/avatar-4.svg',
+  'img/avatar-5.svg',
+  'img/avatar-6.svg'
+];
